d89f4e92c55fb0f688cee66fc8c46cfa168a2e25
Naoki Kimura <n.kimura.cap@gmail.com>
2020-05-24 12:30:50 +0900

    Update @moneyforward/code-review-action

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import stream from 'stream';
 import util from 'util';
-import StaticCodeAnalyzer, { AnalyzerConstructorParameter } from '@moneyforward/sca-action-core';
+import { analyzer } from '@moneyforward/code-review-action';
+import StaticCodeAnalyzer from '@moneyforward/sca-action-core';
 import { transform } from '@moneyforward/stream-util';
+
+type AnalyzerConstructorParameter = analyzer.AnalyzerConstructorParameter;
 
 const debug = util.debuglog('@moneyforward/code-review-action-coffeelint-plugin');
 
@@ -15,7 +18,7 @@ export interface Result {
   }[];
 }
 
-export default class Analyzer extends StaticCodeAnalyzer {
+export default abstract class Analyzer extends StaticCodeAnalyzer {
   constructor(...args: AnalyzerConstructorParameter[]) {
     super('npx', ['-p', '@coffeelint/cli', 'coffeelint'].concat(args.map(String)).concat(['--reporter', 'raw']), undefined, 2, undefined, 'CoffeeLint');
   }
